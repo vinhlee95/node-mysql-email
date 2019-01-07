8a2608715eeb6692df23ba038d38a9514dc12868
vinhle <lethanhvinh95@gmail.com>
2019-01-07 23:42:56 +0200

    write some tets for API routes

---
+++ tests/routes/getUsers.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const app = require('../../app');
+const { expect } = require('chai');
+const faker = require('faker');
+
+describe('GET /', function () {
+	it('shoud get all users', function (done) {
+		request(app)
+			.get('/')
+			.set('Accept', 'application/json')
+			.end(function(err, res) {
+				expect(res.statusCode).to.equal(200);
+				expect(res.body).length.to.have.lengthOf(5);
+				done();
+			});
+	});
+});
+
+const newUser = {
+	email: faker.internet.email()
+}
+describe('POST /', function() {
+	it('show be able to register a new user', function(done) {
+		request(app)
+			.post('/register')
+			.set('Accept', 'application/json')
+			.send(newUser)
+			.end(function(err, res) {
+				console.log(res.body)
+				expect(res.statusCode).to.equal(200);
+				expect(res.body.user.email).to.equal(newUser.email);
+				done();
+			})
+	})
+});
